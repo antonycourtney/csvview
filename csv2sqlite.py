605d28918a1f4e8ce4c37ef6c6402e02c681fe70
Antony Courtney <antony.courtney@gmail.com>
2013-08-30 14:44:15 -0700

    modularization and better two pass column type guessing

--- csv2sqlite.py
+++ csv2sqlite.py
@@ -1,29 +1,45 @@
-#!/usr/bin/python
-"""
+# libraries for loading a CSV file into a sqlite database
 
-Create a sqllite db table from a CSV file.
-Assume every column is a string for now.
-
-"""
-
+import atexit
 import csv
 import os
+import os.path
+import re
 import sqlite3
 import string
-import re
+import sys
+import tempfile
 
-def guessColumnType( vs ):
-    # for now: drop all $ and , chars:
-    cs = vs.translate( None, "$," ).strip()
-    # strip out .:
-    nds = cs.translate( None, ".")
-    if nds.isdigit():
-        if len(nds)==len(cs):
-            return "integer"
-        else:
-            return "real"
-    return "text"
+class ColType:
+    INT = 0
+    REAL = 1
+    TEXT = 2
+    strMap = { INT: 'int', REAL: 'real', TEXT: 'text' }
 
+# compiled regex to match a float or int:
+# Also allows commas and leading $
+intRE = re.compile('[-+]?[$]?[0-9,]+')
+realRE = re.compile('[-+]?[$]?[0-9,]*\.?[0-9]+([eE][-+]?[0-9]+)?')
+
+def guessColumnType( cg, cs ):
+    """Given the current guess (or None) for a column type and cell value string cs
+    make a conservative guess at column type.
+    We use the order int <: real <: text, and a guess will only become more general.
+    TODO: support various date formats....
+"""
+    if cg==ColType.TEXT:
+        return ColType.TEXT
+    if len( cs ) == 0:
+        return cg   # empty cells don't affect current guess
+    if cg==None or cg==ColType.INT:
+        match = intRE.match( cs )
+        if match and match.start()==0 and match.end()==len( cs ):
+            return ColType.INT
+    if cg!=ColType.TEXT:
+        match = realRE.match( cs )
+        if match and match.start()==0 and match.end()==len( cs ):
+            return ColType.REAL
+    return ColType.TEXT
 
 def genColumnIds( colHeaders ):
     """Use descriptive strings from first (header) row of CSV to generate column identifiers for database.
@@ -75,50 +91,65 @@ def createColumnTable( dbConn, tableName, colIdInfo ):
         rowVals = [ id, colName, colDesc ]
         dbConn.execute( insertStmt, rowVals )
 
-filename="salaries_w_names.csv"
-tableName="salaries_w_names"
-# dbName = ":memory:"
-dbName = "testdb.db"
-
-dbConn = sqlite3.connect(dbName)
-with open(filename) as csvfile:
-    rd = csv.reader( csvfile )
-    headerRow = rd.next()
-
-    colIdInfo = genColumnIds( headerRow )
-
-    print "Column ids: ", colIdInfo
-
-    createColumnTable( dbConn, tableName, colIdInfo )
-
-    dataRow0 = rd.next()
-    colNames = map( lambda (cn, _) : cn, colIdInfo )
-    colTypes = map( guessColumnType, dataRow0 )
-
-    # build up Schema string:
-    typedCols = map( lambda cn, ct: "'" + cn + "' " + ct, colNames, colTypes )
-    schemaStr = string.join( typedCols, ", " )
-    dropStr = "DROP TABLE IF EXISTS " + tableName
-    print dropStr
-    dbConn.execute( dropStr )    
-    createStr = "CREATE TABLE " + tableName + " ( " + schemaStr + " )"
-    print createStr
-    dbConn.execute( createStr )
-    qs = ['?'] * len(colNames)
-    insertStmt = "INSERT INTO " + tableName + " VALUES ( " + string.join( qs, ", " ) + " ) "
-    valsRow0 = map(parseType, colTypes, dataRow0 )
-    dbConn.execute( insertStmt, valsRow0 )
-
+def guessColumnTypes( csvfile, rd, nCols  ):
+    """Given a CSV file positioned after header, take a full pass and make a
+conservative guess at column types."""
+    colTypes = [ None ] * nCols 
     for row in rd:
-        rowVals = map( parseType, colTypes, row )
-        dbConn.execute( insertStmt, rowVals )
+        colTypes = map( guessColumnType, colTypes, row )
+    # now lift the column types:
+    def liftCT( ct ):
+        if ct==None:
+            return ColType.TEXT
+        return ct
+    colTypes = map( liftCT, colTypes )
+    colTypes = map( lambda ct: ColType.strMap[ ct ], colTypes )
+    return colTypes
 
-    # dbConn.executemany( insertStmt, rd )
-    dbConn.commit()
 
-query = "select * from " + tableName + "  order by 'Total Compensation' desc limit 5"
-print "Executing ", query, ":" 
-c = dbConn.execute( query )
-for r in c:
-    print r
+def loadCSVFile( dbName, csvFilePath ):
+    """Attempt to open and parse the specified CSV file and load it in to an in-memory sqlite table.
+    Returns:  name of sqllite table
+"""
+    bnm = os.path.basename( csvFilePath )
+    (tableName,_)= os.path.splitext( bnm )
+    dbConn = sqlite3.connect(dbName)
+    with open(csvFilePath) as csvfile:
+        rd = csv.reader( csvfile )
+        headerRow = rd.next()
+        colIdInfo = genColumnIds( headerRow )
+        createColumnTable( dbConn, tableName, colIdInfo )
+        colNames = map( lambda (cn, _) : cn, colIdInfo )
+        colTypes = guessColumnTypes( csvfile, rd, len( colIdInfo ) )
+        # now rewind to beginning of file:
+        csvfile.seek( 0 )
+        rd = csv.reader( csvfile )
+        rd.next()   # skip header
+
+        # build up Schema string:
+        typedCols = map( lambda cn, ct: "'" + cn + "' " + ct, colNames, colTypes )
+        schemaStr = string.join( typedCols, ", " )
+        dropStr = "DROP TABLE IF EXISTS " + tableName
+        dbConn.execute( dropStr )    
+        createStr = "CREATE TABLE " + tableName + " ( " + schemaStr + " )"
+        print createStr
+        dbConn.execute( createStr )
+        qs = ['?'] * len(colNames)
+        insertStmt = "INSERT INTO " + tableName + " VALUES ( " + string.join( qs, ", " ) + " ) "
+        for row in rd:
+            rowVals = map( parseType, colTypes, row )
+            dbConn.execute( insertStmt, rowVals )
+        dbConn.commit()
+    return tableName
+
+def createTempDb():
+    """Create a sqlite database backed by a file in /tmp.  Attempts to remove db on exit."""
+    tf = tempfile.NamedTemporaryFile(prefix='csvview',suffix='.db',dir='/tmp')
+    dbName = tf.name
+    atexit.register( lambda f: f.close(), tf )
+    return dbName
+
+if __name__ == "main":
+    dbName = createTempDb()
+    tableName = loadCSVFile( dbName, sys.argv[1] )
 
